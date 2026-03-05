44379de96169f14ccd42bcc66017d01c8650a96f
“Luiz” <luizcarlosgili@gmail>
2026-03-05 20:46:00 -0300

    feat: bump version

--- docs/python/conf.py
+++ docs/python/conf.py
@@ -14,8 +14,8 @@ sys.path.insert(0, os.path.abspath('../../python/src'))
 project = 'Pulsim Python API'
 copyright = '2024, Pulsim Contributors'
 author = 'Pulsim Contributors'
-version = '0.6.3'
-release = '0.6.3'
+version = '0.6.5'
+release = '0.6.5'
 
 # -- General configuration ---------------------------------------------------
 
